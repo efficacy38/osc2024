d9e56e830db92e3260390dfa29d1cad8506c5346
efficacy38 <efficacy38@gmail.com>
2024-03-03 14:37:14 +0800

    refactor: split code into rust modules

---
+++ rust-version/src/bsp.rs
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+//
+// Copyright (c) 2018-2023 Andre Richter <andre.o.richter@gmail.com>
+
+//! Conditional reexporting of Board Support Packages.
+
+mod raspberrypi;
+
+pub use raspberrypi::*;
--- rust-version/src/main.rs
+++ rust-version/src/main.rs
@@ -1,10 +1,11 @@
 #![no_main]
 #![no_std]
 
-use core::arch::global_asm;
 use core::panic::PanicInfo;
 
-global_asm!(include_str!("boot.S"));
+mod bsp;
+mod cpu;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     unimplemented!()
